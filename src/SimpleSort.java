532058139f9e721b31183ba9877cabae455b9f03
Gabriel <gabrieloliveira280807@gmail.com>
2026-03-17 21:52:22 -0300

    Initial commit

---
+++ src/SimpleSort.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class SimpleSort {
+    public static void main(String[] args) throws IOException {
+
+        int a,b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+
+
+
+    }
+}
